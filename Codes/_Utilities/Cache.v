afc4274de7cd1bcda54e4aebb290e763ba91563f
Amir Ali Arazmjoo <103314770+aaarazm@users.noreply.github.com>
2023-05-22 18:26:35 +0330

    Updating cache and cache controller

--- Codes/_Utilities/Cache.v
+++ Codes/_Utilities/Cache.v
@@ -1,26 +1,27 @@
-module Cache (clk, address, ReadData, WriteData, isHit);
-    input clk;
+module Cache (clk, rst, LRU_update, invalidate, writeEn, address, ReadData, WriteData, isHit);
+    input clk, rst, LRU_update, invalidate, writeEn;
     input [17:0] address;
-    input [31:0] WriteData;
+    input [63:0] WriteData;
 
     output [31:0] ReadData;
     output isHit; // ?
 
-    reg [152:0] cache [0:63];
+    reg [75:0] cache0 [0:63];
+    reg [75:0] cache1 [0:63];
+    reg [63:0] LRU;
 
-    wire offset, LRU, valid0, valid1, hit0, hit1;
+    wire offset, valid0, valid1, hit0, hit1;
     wire [5:0] index;
-    wire [10:0] tag;
+    wire [10:0] tag; // 11 bit tag, 6 bit index, 1 bit offset
     wire [10:0] tag0, tag1;
     wire [63:0] data0, data1;
 
-    reg [152:0] readValue;
 
     assign {tag, index, offset} = address;
 
-    assign readValue = cache[index]; 
     
-    assign {data0, tag0, valid0, data1, tag1, valid1, LRU} = readValue;
+    assign {data0, tag0, valid0} = cache0[index];
+    assign {data1, tag1, valid1} = cache1[index];
 
     assign hit0 = ((tag0 == tag) & valid0) ? 1'b1:1'b0;
     assign hit1 = ((tag1 == tag) & valid1) ? 1'b1:1'b0;
@@ -32,6 +33,32 @@ module Cache (clk, address, ReadData, WriteData, isHit);
                     (hit1 & ~offset) ? data1[31:0]:
                     (hit1 & offset) ? data1[63:32]: 'bz;
 
+    always @(posedge clk) begin
+        if (writeEn & LRU[index]) begin
+            cache0[index] <= {WriteData, tag, 1'b1};
+        end
+        else if (writeEn & ~LRU[index]) begin
+            cache1[index] <= {WriteData, tag, 1'b1};
+        end
+        if (invalidate & hit0) begin
+            valid0 <= 1'b0;
+        end
+        if (invalidate & hit1) begin
+            valid1 <= 1'b0;
+        end
+        if (LRU_update) begin
+            LRU[index] <= (hit0) ? 1'b0:1'b1;
+        end
+    end
+
+    integer i;
+    initial begin
+        for(i=0;i<64;i=i+1)begin
+            cache0[i] = 76'b0;
+            cache1[i] = 76'b0;
+            LRU[i] = 1'b0;
+        end
+    end
     
 
 endmodule
--- Codes/_Utilities/Cache_controller.v
+++ Codes/_Utilities/Cache_controller.v
@@ -1,20 +1,113 @@
 module Cache_controller (
-    clk, rst, address, wdata, MEM_R_EN, MEM_W_EN,
-    rdata, ready, sram_address, sram_wdata, write,
-    sram_rdata, sram_ready
+    clk, rst, address, writeData, MEM_R_EN,
+    MEM_W_EN, ready, sram_readData,
+    sram_ready, sram_address, sram_writeData,
+    SRAM_readEn, SRAM_writeEn, isHit,
+    cache_readData, cache_writeEn, cache_writeData,
+    cache_address, LRU_update, invalidate
 );
     input clk, rst;
 
     // memory stage unit
-    input [31:0] address, wdata;
+    input [31:0] address, writeData;
     input MEM_R_EN, MEM_W_EN;
-    output [31:0] rdata;
     output ready;
 
     //SRAM Ctrl
-    input [31:0] sram_rdata;
+    input [31:0] sram_readData;
     input sram_ready;
-    output [31:0] sram_address, sram_wdata;
-    output write;
+    output [31:0] sram_address, sram_writeData;
+    output reg SRAM_readEn, SRAM_writeEn;
+
+    //Cache
+    input isHit; // ??
+    input[31:0] cache_readData;
+
+    output reg cache_writeEn;
+    output[63:0] cache_writeData;
+    output[17:0] cache_address;
+    output LRU_update;
+    output reg invalidate;
+
+
+    reg [31:0] dataLSB, dataMSB;
+    reg loadLSB, loadMSB, offset;
+
+    assign sram_writeData = writeData;
+    assign sram_address = {address[31:3], offset, 2'b0};
+    assign cache_address = address[19:2];
+    assign cache_writeData = {dataMSB, dataLSB};
+    assign LRU_update = (MEM_R_EN & isHit) ? 1'b1:1'b0;
+    assign ready = 
+
+
+    reg [2:0] ps, ns;
+
+    localparam [2:0] idle = 0, writeState = 1, readState1 = 2, readState2 = 3, cache_writeBack = 4;
+
+    always @* begin
+        case (ps)
+            idle: begin
+                ns = (MEM_W_EN) ? writeState:
+                    (MEM_R_EN & ~isHit) ? readState1:idle;
+            writeState: begin
+                ns = (sram_ready) ? idle:writeState;
+            end 
+            readState1: begin
+                ns = (sram_ready) ? readState2:readState1;
+            end
+            readState2: begin
+                ns = (sram_ready) ? cache_writeBack:readState2;
+            end
+            cache_writeBack: begin
+                ns = idle;
+            end
+            end
+            default: ns = idle;
+        endcase
+    end
+
+    always @(ps, ns) begin
+        {invalidate, cache_writeEn, loadMSB, loadLSB, offset, SRAM_writeEn, SRAM_readEn} = 7'b0;
+        case (ps)
+            idle: begin
+                invalidate = (ns == writeState) ? 1'b1:1'b0;
+                SRAM_writeEn = (ns == writeState) ? 1'b1:1'b0;
+                SRAM_readEn = (ns == readState1) ? 1'b1:1'b0;
+            end
+            readState1: begin
+                loadLSB = (ns == readState2) ? 1'b1:1'b0;
+            end
+            readState2: begin
+                loadMSB = (ns == cache_writeBack) ? 1'b1:1'b0;
+                offset = 1'b1;
+            end
+            cache_writeBack: begin
+                cache_writeEn = 1'b1;
+            end
+            default: {invalidate, cache_writeEn, loadMSB, loadLSB, offset, SRAM_writeEn, SRAM_readEn} = 7'b0;
+        endcase
+    end
+
+    always @(posedge clk) begin
+        if (rst) begin
+            dataLSB <= 32'b0
+            dataMSB <= 32'b0
+        end
+        else if (loadLSB) begin
+            dataLSB <= sram_readData;
+        end
+        else if (loadMSB) begin
+            dataMSB <= sram_readData;
+        end
+    end
+
+    always @(posedge clk) begin
+        if (rst)
+            ps <= idle;
+        else
+            ps <= ns;
+    end
+
 
 endmodule
